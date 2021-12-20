a9d6b7ef0cdf6405e30b04569e5972ff2de983a5
Michael Baudino <michael@baudi.no>
2021-12-20 22:53:10 +0100

    Rename `RUBY_VERSION` and `ALIAS` environment variable to `RUBY_IMAGE_TAG` and `ALIAS_TAGS`
    For better explicitness and to avoid conflicts with Travis-provided environment variables.

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-if [ -z "${RUBY_VERSION}" ]; then
-  echo "Error: \$RUBY_VERSION is unset. Exiting."
+if [ -z "${RUBY_IMAGE_TAG}" ]; then
+  echo "Error: \$RUBY_IMAGE_TAG is unset. Exiting."
   exit 64
 fi
 
 set -ev
 
-docker pull ruby:${RUBY_VERSION} # (needlessly mandatory to prevent a bug in Travis)
+docker pull ruby:${RUBY_IMAGE_TAG} # (needlessly mandatory to prevent a bug in Travis)
 
 docker build . \
-  --build-arg BASE_IMAGE_TAG=${RUBY_VERSION} \
-  --tag alpinelab/ruby-dev:${RUBY_VERSION} \
+  --build-arg BASE_IMAGE_TAG=${RUBY_IMAGE_TAG} \
+  --tag alpinelab/ruby-dev:${RUBY_IMAGE_TAG} \
   $(
-    for alias in ${ALIAS//,/ }; do
+    for alias in ${ALIAS_TAGS//,/ }; do
       echo "--tag alpinelab/ruby-dev:${alias}"
     done
   )
--- publish.sh
+++ publish.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-if [ -z "${RUBY_VERSION}" ]; then
-  echo "Error: \$RUBY_VERSION is unset. Exiting."
+if [ -z "${RUBY_IMAGE_TAG}" ]; then
+  echo "Error: \$RUBY_IMAGE_TAG is unset. Exiting."
   exit 64
 fi
 
 set -ev
 
 # Publish default name
-docker push alpinelab/ruby-dev:${RUBY_VERSION}
+docker push alpinelab/ruby-dev:${RUBY_IMAGE_TAG}
 
 # Publish aliases
-for alias in ${ALIAS//,/ }; do
+for alias in ${ALIAS_TAGS//,/ }; do
   docker push alpinelab/ruby-dev:${alias}
 done
